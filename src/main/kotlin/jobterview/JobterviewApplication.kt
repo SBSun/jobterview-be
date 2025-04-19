db34c43b6ca1e6301bf7bef0c3bf04587aee5898
SBSun <byung0216@gmail.com>
2025-04-19 21:58:25 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/kotlin/jobterview/JobterviewApplication.kt
@@ -0,0 +1,11 @@
+package jobterview
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobterviewApplication
+
+fun main(args: Array<String>) {
+	runApplication<JobterviewApplication>(*args)
+}
---
+++ src/test/kotlin/jobterview/jobterview/JobterviewApplicationTests.kt
@@ -0,0 +1,13 @@
+package jobterview.jobterview
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobterviewApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
