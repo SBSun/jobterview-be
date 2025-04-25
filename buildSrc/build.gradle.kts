ceaeb8364ab508ca2e6b3b6c53f6173deb9b9222
SBSun <byung0216@gmail.com>
2025-04-25 21:30:25 +0900

    chore: 누락된 build.gradle 파일 추가 (#11)

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    `kotlin-dsl`
+}
+
+val kotlinVersion: String by project
+val springBootVersion: String by project
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-noarg:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
+
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
+    implementation("io.spring.gradle:dependency-management-plugin:1.1.3")
+    implementation("org.asciidoctor:asciidoctor-gradle-jvm:3.3.2")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:11.5.0")
+    implementation("net.saliman:gradle-properties-plugin:1.5.2")
+}
