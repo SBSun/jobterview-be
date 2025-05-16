d07ddd3e5ae98468b8332a66dc631c6c320f1565
SBSun <byung0216@gmail.com>
2025-05-16 10:26:21 +0900

    feat: LogUtil 구현 (#46)

--- jobterview-mq/build.gradle.kts
+++ jobterview-mq/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":jobterview-common"))
     api(project(":jobterview-mail"))
 
     api("org.springframework.boot:spring-boot-starter-amqp")
